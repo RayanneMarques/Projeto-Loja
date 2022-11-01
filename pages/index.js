f021556699cae34845c83957ede9f696abcb3532
lararaujo19 <lara.araujo20@outlook.com>
2022-11-01 17:43:09 -0300

    implementação da home page

--- pages/index.js
+++ pages/index.js
@@ -4,5 +4,28 @@ import Banner from "../public/imgs/cara3.png";
 import HomeStoreList from "../components/HomeStoreList";
 
 export default function Home() {
-  return <>Home</>;
+  return <>
+  <Row className="justify-content-center pt-5">
+    <Col 
+    md={6}
+    style= {{borderRadius:"24px",
+         backgroundColor:"	#2596be"}}>
+    <Row className="text-white align-items-center mt-3">
+      <Col>
+      <Image src={Banner}/> 
+      </Col>
+      <Col>
+      <h2>MarketPlace</h2>
+      <h3> Venha comprar conosco!</h3>
+      
+      </Col>
+    </Row>
+    </Col>
+  </Row>
+  <div className="text-center p-5">
+    <h1>Nossos Clientes:</h1>
+  </div>
+  <HomeStoreList/>
+  </>;
 }
+
